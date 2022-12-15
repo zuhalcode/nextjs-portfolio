068459eb6a5cbe08989329870f9a2b6f9ed49d43
zuhalcode <zuhalcode@gmail.com>
2022-12-15 10:56:34 +0700

    tes sqlae

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ export default function Home() {
   return (
     <>
       <div className="text-blue-500">zuhal</div>
-      <div className="text-red-500">ganteng</div>;
+      <div className="text-red-500">ganteng</div>
+      <div className="text-red-500">sqalee</div>
     </>
   );
 }
