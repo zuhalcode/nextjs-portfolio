0542767e73787e8c72f141738dd352ba0434c7a0
zuhalcode <zuhalcode@gmail.com>
2022-12-15 10:55:21 +0700

    tes zuhal

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,9 @@
 export default function Home() {
-  return <div className="text-blue-500">zuhal</div>;
+  return (
+    <>
+      <div className="text-blue-500">zuhal</div>
+      <div className="text-red-500">ganteng</div>;
+    </>
+  );
 }
 
